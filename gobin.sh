d080232f2a13e9ceba386ce5102fe9d987630cff
tuxudong <tuxudong@jd.com>
2025-03-18 21:32:59 +0800

    改造最新版，下一步提升性能

--- gobin.sh
+++ gobin.sh
@@ -1,3 +1,3 @@
-wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.23.6.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.6.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc && source ~/.bashrc
