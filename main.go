28f41f630ea0086eb4a354c83a8f58c6dcca43af
tuxudong <tuxudong@jd.com>
2025-03-18 22:26:14 +0800

    bug

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func createTpccDatabase() *memory.Database {
 	dbName := "tpcc"
 	db := memory.NewDatabase(dbName)
 
-	sqlFiles :=string{"tpcc-mysql/create_table.sql", "tpcc-mysql/add_fkey_idx.sql"} // Assuming these files are in same directory
+	sqlFiles := []string{"tpcc-mysql/create_table.sql", "tpcc-mysql/add_fkey_idx.sql"} // Assuming these files are in same directory
 	ctx := sql.NewEmptyContext()
 	e := sqle.NewDefault(sql.NewDatabaseProvider(db, information_schema.NewInformationSchemaDatabase()))
 
