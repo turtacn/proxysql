0a428d1529f6b24012b0ab49ce4a46fdf2809c98
tuxudong <tuxudong@jd.com>
2025-03-20 15:37:45 +0800

    bug fix1

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/information_schema"
 	"log"
 	"os"
 	"strings"
@@ -10,24 +12,37 @@ import (
 	sqle "github.com/dolthub/go-mysql-server"
 	"github.com/dolthub/go-mysql-server/memory"
 	"github.com/dolthub/go-mysql-server/server"
-	"github.com/dolthub/go-mysql-server/sql"
-	"github.com/dolthub/go-mysql-server/sql/information_schema"
 )
 
 func main() {
-	tpccDB := createTpccDatabase()
-	databaseProvider := sql.NewDatabaseProvider(
-		tpccDB,
-		information_schema.NewInformationSchemaDatabase(),
+	var (
+		username = "root"
+		password = "123"
+		host     = "localhost"
+		dbname   = "tpcc"
+		port     = 3306
+		err      error
 	)
-	engine := sqle.NewDefault(databaseProvider)
 
+	db := createTpccDatabase(dbname)
+
+	db.BaseDatabase.EnablePrimaryKeyIndexes()
+	provider := memory.NewDBProvider(db)
+	engine := sqle.NewDefault(provider)
+	mysqlDb := engine.Analyzer.Catalog.MySQLDb
+	mysqlDb.SetEnabled(true)
+	mysqlDb.AddRootAccount()
 	config := server.Config{
 		Protocol: "tcp",
-		Address:  "localhost:3306",
+		Address:  fmt.Sprintf("%s:%d", host, port),
 	}
 
-	s, err := server.NewServer(config, engine, memory.NewSessionBuilder(memory.NewDBProvider(tpccDB)), nil)
+	s, err := server.NewServer(
+		config,
+		engine,
+		memory.NewSessionBuilder(provider),
+		nil,
+		)
 	if err != nil {
 		panic(err)
 	}
@@ -37,54 +52,7 @@ func main() {
 		log.Fatalf("Server error: %v", err)
 	}
 }
-
-func createTpccDatabase() *memory.Database {
-	dbName := "tpcc"
-	db := memory.NewDatabase(dbName)
-
-	sqlFiles := []string{"tpcc-mysql/create_table.sql", "tpcc-mysql/add_fkey_idx.sql"} // Assuming these files are in same directory
-	ctx := sql.NewEmptyContext()
-	e := sqle.NewDefault(sql.NewDatabaseProvider(db, information_schema.NewInformationSchemaDatabase()))
-
-	_, _, _, err := e.Query(ctx, fmt.Sprintf("CREATE DATABASE %s;", dbName))
-	if err != nil {
-		panic(fmt.Sprintf("Error creating database %s: %v", dbName, err))
-	}
-
-	_, _, _, err = e.Query(ctx, fmt.Sprintf("USE %s;", dbName))
-	if err != nil {
-		panic(fmt.Sprintf("Error using database %s: %v", dbName, err))
-	}
-
-	for _, file := range sqlFiles {
-		sqlContent, err := os.ReadFile(file)
-		if err != nil {
-			panic(fmt.Sprintf("Error reading SQL file %s: %v. Please make sure %s and add_fkey_idx.sql are in the same directory as main.go", file, err, "create_table.sql"))
-		}
-		queries := strings.Split(string(sqlContent), ";")
-
-		for _, query := range queries {
-			query = strings.TrimSpace(query)
-			fmt.Println(query)
-			if query == "" {
-				continue
-			}
-
-			_, _, _, err = e.Query(ctx, query+";")
-			if err != nil {
-				fmt.Printf("Error executing SQL: %s\n", query) // Print the failing query
-				panic(fmt.Sprintf("Error executing SQL from %s: %v", file, err))
-			}
-		}
-		fmt.Printf("Executed SQL from %s\n", file)
-	}
-	fmt.Println("TPCC database and tables created.")
-	return db
-}
-
-/*
-func createTpccDatabase() *memory.Database {
-	dbName := "tpcc"
+func createTpccDatabase(dbName string) *memory.Database {
 	db := memory.NewDatabase(dbName)
 
 	sqlFiles := []string{"tpcc-mysql/create_table.sql", "tpcc-mysql/add_fkey_idx.sql"} // Assuming these files are in same directory
@@ -120,4 +88,3 @@ func createTpccDatabase() *memory.Database {
 	fmt.Println("TPCC database and tables created.")
 	return db
 }
-*/
