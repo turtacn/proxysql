6c10ce69a6f43ee9117922ca24bb715f4d97c0a4
tuxudong <tuxudong@jd.com>
2025-03-23 19:08:46 +0800

    load more with more parameters

--- tpcc-mysql/load_more.sh
+++ tpcc-mysql/load_more.sh
@@ -1,35 +1,32 @@
+#!/bin/bash
+# 设置动态库搜索路径
 export LD_LIBRARY_PATH=/usr/local/mysql/lib/mysql/
+
+# 外部参数：
+# $1: 数据库名称（必选）
+# $2: WH 参数（必选）
+# $3: STEP 值（可选，默认1）
+# $4: 并发数，即 tpcc_load 的 level 数量（可选，默认4）
 DBNAME=$1
 WH=$2
-HOST=127.0.0.1
-STEP=5
+STEP=${3:-1}
+CONCURRENCY=${4:-4}
 
+HOST=127.0.0.1
+
+# 首先调用 level=1 的 tpcc_load，范围为 1 到 WH，输出到 1.out
 ./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 1 -m 1 -n $WH >> 1.out &
 
 x=1
 
+# 循环处理范围分段，步长为 STEP
 while [ $x -le $WH ]
 do
- echo $x $(( $x + $STEP - 1 ))
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 2 -m $x -n $(( $x + $STEP - 1 ))  >> 2_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 3 -m $x -n $(( $x + $STEP - 1 ))  >> 3_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 4 -m $x -n $(( $x + $STEP - 1 ))  >> 4_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 5 -m $x -n $(( $x + $STEP - 1 ))  >> 5_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 6 -m $x -n $(( $x + $STEP - 1 ))  >> 6_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 7 -m $x -n $(( $x + $STEP - 1 ))  >> 7_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 8 -m $x -n $(( $x + $STEP - 1 ))  >> 8_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 9 -m $x -n $(( $x + $STEP - 1 ))  >> 9_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 10 -m $x -n $(( $x + $STEP - 1 ))  >> 10_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 11 -m $x -n $(( $x + $STEP - 1 ))  >> 11_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 12 -m $x -n $(( $x + $STEP - 1 ))  >> 12_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 13 -m $x -n $(( $x + $STEP - 1 ))  >> 13_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 14 -m $x -n $(( $x + $STEP - 1 ))  >> 14_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 15 -m $x -n $(( $x + $STEP - 1 ))  >> 15_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 16 -m $x -n $(( $x + $STEP - 1 ))  >> 16_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 17 -m $x -n $(( $x + $STEP - 1 ))  >> 17_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 18 -m $x -n $(( $x + $STEP - 1 ))  >> 18_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 19 -m $x -n $(( $x + $STEP - 1 ))  >> 19_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 20 -m $x -n $(( $x + $STEP - 1 ))  >> 20_$x.out &
-./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l 21 -m $x -n $(( $x + $STEP - 1 ))  >> 21_$x.out &
- x=$(( $x + $STEP ))
+  echo "处理范围: $x 到 $(( x + STEP - 1 ))"
+  # 根据并发参数 CONCURRENCY，从 level=2 开始依次调用
+  for level in $(seq 2 $CONCURRENCY); do
+    ./tpcc_load -h $HOST -d $DBNAME -u root -p "" -w $WH -l $level -m $x -n $(( x + STEP - 1 )) >> ${level}_$x.out &
+  done
+  # 更新下一个区间
+  x=$(( x + STEP ))
 done
